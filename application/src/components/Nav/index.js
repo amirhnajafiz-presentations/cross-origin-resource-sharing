47f6d4e51fa336c67092b4d9c42d1b575ff2860e
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-01 14:06:12 +0330

    add: nav

---
+++ application/src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <div className={"nav"}>
+                <h4>
+                    IP validator
+                </h4>
+                <h4>
+                    Time
+                </h4>
+            </div>
+        )
+    }
+}
+
+export default Nav;
--- application/src/containers/App/index.js
+++ application/src/containers/App/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { MyForm } from '../../components/Form'
+import { MyForm } from '../../components/Form';
+import Nav from '../../components/Nav';
+import './index.css';
 
 class App extends React.Component {
     render() {
       return (
-          <div>
+          <div className={"container"}>
+              <Nav />
               <MyForm />
           </div>
       )
