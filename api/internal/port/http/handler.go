b2d4efab8882d119d4f8be544d3e26513c7f0a32
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-01 11:01:59 +0330

    add: handler

--- api/internal/port/http/handler.go
+++ api/internal/port/http/handler.go
@@ -1,1 +1,17 @@
 package http
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler struct{}
+
+func (h *Handler) getTime(ctx *fiber.Ctx) error {
+	t := time.Now()
+	s := fmt.Sprintf("%02d:%02d:%02d %s", t.Hour(), t.Minute(), t.Second(), t.Location().String())
+
+	return ctx.SendString(s)
+}
