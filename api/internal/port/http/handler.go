ca93119c9a7aac3b91263bdf9730a1eed4170501
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-03 15:40:57 +0330

    feat: new handler methods

--- api/internal/port/http/handler.go
+++ api/internal/port/http/handler.go
@@ -1,10 +1,24 @@
 package http
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 type Handler struct{}
 
+func (h *Handler) Namespace(ctx *fiber.Ctx) error {
+	// todo: make http request to get repositories or tags
+	return ctx.SendString("hello world")
+}
+
+func (h *Handler) Health(ctx *fiber.Ctx) error {
+	return ctx.SendString(fmt.Sprintf("OK\n\t%s", time.Now().String()))
+}
+
 func (h *Handler) Register(app *fiber.App) {
+	app.Get("/api/health", h.Health)
+	app.Get("/api/namespace/:namespace/repository/:repository/tags", h.Namespace)
 }
