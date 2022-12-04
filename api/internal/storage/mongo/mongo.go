f9a13faf329e0533059a664d95d292b4257567cb
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-04 09:17:02 +0330

    add: mongodb connection module

--- api/internal/storage/mongo/mongo.go
+++ api/internal/storage/mongo/mongo.go
@@ -1,1 +1,33 @@
 package mongo
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// NewConnection
+// opens a new connection to mongodb database.
+func NewConnection(mongoURI string, database string) (*mongo.Database, error) {
+	// mongodb server options
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+
+	clientOptions := options.Client().
+		ApplyURI(mongoURI).
+		SetServerAPIOptions(serverAPIOptions)
+
+	// creating mongodb client
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("mongoDB connection failed: %w", err)
+	}
+
+	// ping mongodb
+	if er := client.Ping(context.TODO(), nil); er != nil {
+		return nil, fmt.Errorf("mongoDB ping failed: %w", er)
+	}
+
+	return client.Database(database), nil
+}
