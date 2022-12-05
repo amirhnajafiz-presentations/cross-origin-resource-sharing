4ebf86777242f0974d1636a581d80a628bc0cfa1
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-05 22:43:54 +0330

    feat: connecting to github api

--- api/internal/port/http/handler.go
+++ api/internal/port/http/handler.go
@@ -4,17 +4,22 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/frontend-developing/cross-origin-resource-sharing/api/internal/model"
-	"go.mongodb.org/mongo-driver/bson"
+	"io"
+	"net/http"
 	"time"
 
+	"github.com/frontend-developing/cross-origin-resource-sharing/api/internal/model"
+
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
 	errEmptyUser    = errors.New("user cannot be empty")
 	errParsingModel = errors.New("parsing model failed")
+	errHttpRequest  = errors.New("cannot create http request")
+	errGitHub       = errors.New("cannot connect to github server")
 )
 
 // Handler manages the http endpoint methods.
@@ -37,11 +42,39 @@ func (h *Handler) UserGithubRepos(ctx *fiber.Ctx) error {
 
 	// check to see that if we have user in mongo or not
 	if res := h.Mongo.Collection("users").FindOne(ctx.Context(), filter, nil); res.Err() == nil {
+		// parsing the result
 		if err := res.Decode(&userModel); err != nil {
 			return errParsingModel
 		}
 	} else {
+		// creating http request
+		req, err := http.NewRequest(
+			"GET",
+			fmt.Sprintf("https://api.github.com/users/%s/repos", user),
+			nil,
+		)
+		if err != nil {
+			return errHttpRequest
+		}
 
+		// creating http client
+		client := http.Client{}
+
+		// getting the response
+		resp, err := client.Do(req)
+		if err != nil {
+			return errGitHub
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("github response error: %v", err)
+		}
+
+		// getting the response body
+		body, _ := io.ReadAll(resp.Body)
+
+		userModel.Name = user
+		userModel.Value = string(body)
 	}
 
 	// creating a new response
