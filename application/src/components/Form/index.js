cf121f1d8b82cff7452b539c88fa5d3bd09410ce
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-06 19:45:15 +0330

    fix: forms

--- application/src/components/Form/index.js
+++ application/src/components/Form/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import './index.css';
 
+// Form component manages to handle the form submit.
 class Form extends React.Component {
-    state = { user: '' }
+    constructor(props) {
+        super(props);
 
+        // setting the component state
+        this.state = {
+            user: ""
+        }
+
+        // binding the submit method
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    // handle submits
     handleSubmit(event) {
         event.preventDefault();
 
@@ -14,23 +26,31 @@ class Form extends React.Component {
         alert(`Input:\n${this.state.user}`);
     }
 
+    // change user
     changeUser(event) {
-        this.setState({user: event.target.value})
+        // setting the user new value
+        this.setState({
+            user: event.target.value
+        })
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit} className={"main_form"}>
-                <label style={{display: "block"}}>
-                    <span className={"main_label"}>Enter name:</span>
+            <form
+                onSubmit={this.handleSubmit}
+            >
+                <label>
+                    <span>Enter name:</span>
                     <input
-                        className={"input_form"}
                         type="text"
-                        value={this.state.user}
+                        value={this.state.use}
                         onChange={(e) => this.changeUser(e)}
                     />
                 </label>
-                <input className={"main_submit"} type="submit" value="Send" />
+                <input
+                    type="submit"
+                    value="Send"
+                />
             </form>
         )
     }
