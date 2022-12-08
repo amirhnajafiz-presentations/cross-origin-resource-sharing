0365da2b0e5da984376bbed257e23370b2c0beb7
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-08 23:12:18 +0330

    fix: user repo erros

--- application/src/components/Box/index.js
+++ application/src/components/Box/index.js
@@ -17,45 +17,50 @@ class Box extends React.Component {
 
         return(
             <div className={"response"}>
-                  {
-                      array.map((repo) => (
-                          <div className={"row"}>
-                              <h3>
-                                  {repo.full_name}
-                              </h3>
-                              <p className={"row-des"}>
-                                  {repo.description}
-                              </p>
-                              <ul className={"list"}>
-                                  {
-                                      repo.topics.map((topic) => (
-                                          <li className={"list-item"}>
-                                              {topic}
-                                          </li>
-                                      ))
-                                  }
-                              </ul>
-                              <div className={"row-opt"}>
-                                  <small>
-                                      {repo.language}
-                                  </small>
-                                  <small>
-                                      Stars: {repo.stargazers_count}
-                                  </small>
-                                  <small>
-                                      <a
-                                          className={"row-link"}
-                                          href={repo.html_url}
-                                          target={"_blank"}
-                                          rel={"noreferrer"}
-                                      >
-                                        Repo link
-                                      </a>
-                                  </small>
-                              </div>
-                          </div>
-                      ))
-                  }
+                {
+                    this.props.userInformation !== "No value" ? (
+                        array.map((repo) => (
+                            <div key={repo.id} className={"row"}>
+                                <h3>
+                                    {repo.full_name}
+                                </h3>
+                                <p className={"row-des"}>
+                                    {repo.description}
+                                </p>
+                                <ul className={"list"}>
+                                    {
+                                        repo.topics.map((topic) => (
+                                            <li className={"list-item"}>
+                                                {topic}
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                                <div className={"row-opt"}>
+                                    <small>
+                                        {repo.language}
+                                    </small>
+                                    <small>
+                                        Stars: {repo.stargazers_count}
+                                    </small>
+                                    <small>
+                                        <a
+                                            className={"row-link"}
+                                            href={repo.html_url}
+                                            target={"_blank"}
+                                            rel={"noreferrer"}
+                                        >
+                                            Repo link
+                                        </a>
+                                    </small>
+                                </div>
+                            </div>
+                        ))
+                ) : (
+                    <pre>
+                        No value
+                    </pre>
+                )}
             </div>
         )
     }
